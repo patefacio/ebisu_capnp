6b19ab34b74cac78172aa27191b81399437e1ce9
Daniel Davidson <dbdavidson@yahoo.com>
2015-11-07 16:22:51 -0600

    added naming tests for constants

---
+++ test/src/test_factories/name_constants.dart
@@ -0,0 +1,18 @@
+part of ebisu_capnp.test_factories;
+
+// custom <part name_constants>
+
+final _constNameConversions = {
+  const_('color', int32T, '32'): 'const Color :Int32 = 32;',
+};
+
+testNamingConsts() {
+  _constNameConversions.forEach((dynamic entry, String value) {
+    test('${entry.runtimeType}-${entry.name}', () {
+      _logger.info(entry.definition);
+      expect(darkSame(entry.definition, value), true);
+    });
+  });
+}
+
+// end <part name_constants>
